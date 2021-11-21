992d620acc6c064569b20724daea3c10b453dec8
fm102dx <fm102dx@gmail.com>
2021-11-21 20:02:18 +0300

    Initial commit

---
+++ ExampleApp/BusinessObjects.cs
@@ -0,0 +1,174 @@
+﻿using CommonFunctions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using FerryActiveObjectsClassLibrary;
+using System.Threading;
+
+namespace Ferry02_Example
+{
+    //бизнес-объекты
+
+    public class TextWriter : ActiveObject
+    {
+        //прсото посылает рандомный текст при получении сообщения
+        string targetUrl;
+        Random random = new Random();
+        public override string objectType { get { return "TextWriter"; } }
+
+        public TextWriter(Scenario _scenario) : base(_scenario)
+        {
+            variableContext.createVariable("a01_int", VariableTypeEnum.Int);
+            variableContext.createVariable("b01_string", VariableTypeEnum.String);
+        }
+
+        public override void processIncomingMessage(IInterObjectMessage msg)
+        {
+            string txt="";
+            txt = $"Текст1={random.Next(10)}";
+            Logger.log("", "Writer writes: "+txt);
+            variableContext.setVariableValue("a01_int", random.Next(10));
+            variableContext.setVariableValue("b01_string", $"Строка: {random.Next(10)}");
+
+            //Присвоить значение переменной
+            sendMyMessage(new InterObjectMessage(ObjectMessageTypeEnum.ActivationMsg, guid, txt));
+        }
+    }
+
+    public class HttpDataConnector : ActiveObject
+    {
+        //коннектор
+        //ну, коннектор это штуковина, которая ходит не ресурс и делает там запрос
+        string targetUrl;
+
+        public override string objectType { get { return "HttpDataConnector"; } }
+
+        public HttpDataConnector(string _objectId, Scenario _scenario, string _targetUrl) : base(_scenario)
+        {
+            targetUrl = _targetUrl;
+        }
+
+        public override void processIncomingMessage(IInterObjectMessage msg)
+        {
+            //здесь ходим на URL 
+            string jsonRezult = "";
+            string url = string.Format(@""+ targetUrl);
+            try
+            {
+                //Logger.log("en");
+                var request = WebRequest.Create(url);
+                using (var response = (HttpWebResponse)request.GetResponse())
+                using (var stream = response.GetResponseStream())
+                using (var reader = new StreamReader(stream))
+                {
+                    jsonRezult = reader.ReadToEnd();
+                }
+                
+                sendMyMessage( new InterObjectMessage( ObjectMessageTypeEnum.ActivationMsg,guid, jsonRezult) );
+            }
+            catch
+            {
+
+            }
+        }
+
+
+    }
+
+    public class FerryLogger01 : ActiveObject,IActiveObject
+    {
+        //логгер - штуковина, которая логгирует 
+
+        public FerryLogger01(Scenario _scenario, string _fileName, string _alias, ConsoleColor _color= ConsoleColor.White, DynamicLogger.LogDirectionEnum logDirection= DynamicLogger.LogDirectionEnum.toConsole) : base(_scenario)
+        {
+            fileName = _fileName;
+            dynamicLogger = new DynamicLogger(_fileName, _alias, logDirection);
+            color = _color;
+        }
+        ConsoleColor color;
+        public override string objectType { get { return "FerryLogger01"; } }
+
+        DynamicLogger dynamicLogger;
+
+        string fileName;
+
+        public override void processIncomingMessage(IInterObjectMessage msg)
+        {
+            Console.ForegroundColor = color;
+            try
+            {
+                CommonFunctions.Logger.log($"{dynamicLogger.alias}: ", $"{msg.textContent}");
+
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Error: {ex.Message}");
+                Console.ForegroundColor = color;
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+
+    public class PeriodicTimer : ActiveObject
+    {
+        public PeriodicTimer(string _objectId, Scenario _scenario, int periodMs) : base(_scenario, false)
+        {
+            int num = 0;
+            tm = new TimerCallback(TimerProcessor);
+            timer = new Timer(tm, num, 0, periodMs);
+        }
+        Timer timer;
+
+        TimerCallback tm;
+        public override string objectType { get { return "PeriodicTimer"; } }
+        public override void processIncomingMessage(IInterObjectMessage msg)
+        {
+            //таймер не обрабатывает входящие сообщения
+        }
+
+        void TimerProcessor(object obj)
+        {
+            Console.WriteLine(""); 
+            Console.WriteLine("-----------TimerTeak-----------"); 
+            InterObjectMessage msg = new InterObjectMessage(ObjectMessageTypeEnum.ActivationMsg,guid, "");
+            sendMyMessage(msg);
+        }
+    }
+
+
+
+    public class RndIntGenerator : ActiveObject
+    {
+        //прсото посылает рандомный текст при получении сообщения
+        string targetUrl;
+        Random random = new Random();
+        public override string objectType { get { return "RndIntGenerator"; } }
+
+        public RndIntGenerator(Scenario _scenario) : base(_scenario)
+        {
+            variableContext.createVariable("myDigit", VariableTypeEnum.Int);
+            variableContext.createVariable("isEven", VariableTypeEnum.Bool);
+        }
+
+        public override void processIncomingMessage(IInterObjectMessage msg)
+        {
+            int myDigit = random.Next(100);
+            bool isEven = (myDigit % 2 == 0);
+            string evenText = isEven ? "Even" : "Odd";
+            string textContent = $"Number is {myDigit} and it's {evenText}";
+            Logger.log("", $"RndIntGenerator produced digit={myDigit} and it's {evenText}");
+            
+            variableContext.setVariableValue("myDigit", myDigit);
+
+            variableContext.setVariableValue("isEven", isEven);
+
+            //Присвоить значение переменной
+            sendMyMessage(new InterObjectMessage(ObjectMessageTypeEnum.ActivationMsg, guid, textContent));
+        }
+    }
+
+}
---
+++ ExampleApp/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+using CommonFunctions;
+using FerryActiveObjectsClassLibrary;
+
+namespace Ferry02_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            App app = new App();
+            app.start02();
+        }
+    }
+
+
+    public class App
+    {
+
+        public void start01()
+        {
+            Console.WriteLine("Starting app");
+
+            Scenario s = new Scenario();
+
+            s.Stop();
+
+            PeriodicTimer periodicTimer = new PeriodicTimer("", s, 3000);
+            s.AddScenarioObject(periodicTimer);
+
+            TextWriter textWriter = new TextWriter(s);
+            s.AddScenarioObject(textWriter);
+
+            /*
+            HttpDataConnector connector01 = new HttpDataConnector("", s, @"https://develop.ricompany.info/api/aqualionbot");
+            s.AddScenarioObject(connector01);
+
+            HttpDataConnector connector02 = new HttpDataConnector("", s, @"https://develop.ricompany.info/api/aqualionbot");
+            s.AddScenarioObject(connector02);
+            */
+
+            FerryLogger01 logger = new FerryLogger01(s, @"FerryLoggerLogFile.txt", "Logger");
+            s.AddScenarioObject(logger);
+
+            s.subscriptionManager.AddSubscription(periodicTimer, textWriter);
+            s.subscriptionManager.AddSubscription(textWriter, logger);
+
+            /*
+            s.subscriptionManager.AddSubscription(periodicTimer, connector01);
+            s.subscriptionManager.AddSubscription(periodicTimer, connector02);
+            s.subscriptionManager.AddSubscription(connector01, logger);
+            s.subscriptionManager.AddSubscription(connector02, logger);
+            */
+
+
+
+            s.Run();
+
+            Console.ReadLine();
+
+        }
+
+        public void start02()
+        {
+            Console.WriteLine("Starting app");
+
+            Scenario s = new Scenario();
+
+            s.Stop();
+
+            PeriodicTimer periodicTimer = new PeriodicTimer("", s, 2500);
+            s.AddScenarioObject(periodicTimer);
+
+            RndIntGenerator rndIntGenerator = new RndIntGenerator(s);
+            s.AddScenarioObject(rndIntGenerator);
+
+            //логгирует четные
+            FerryLogger01 evenLogger = new FerryLogger01(s, @"FerryLoggerLogFile.txt", "EvenLogger", ConsoleColor.Cyan);
+            s.AddScenarioObject(evenLogger);
+
+            //логгирует нечетные
+            FerryLogger01 oddLogger1 = new FerryLogger01(s, @"FerryLoggerLogFile.txt", "OddLogger01", ConsoleColor.Yellow);
+            s.AddScenarioObject(oddLogger1);
+
+            FerryLogger01 oddLogger2 = new FerryLogger01(s, @"FerryLoggerLogFile.txt", "OddLogger02", ConsoleColor.Yellow);
+            s.AddScenarioObject(oddLogger2);
+
+            FerryLogger01 oddLogger3 = new FerryLogger01(s, @"FerryLoggerLogFile.txt", "OddLogger03", ConsoleColor.Yellow);
+            s.AddScenarioObject(oddLogger3);
+
+            Hub hub = new Hub(s);
+            s.AddScenarioObject(hub);
+
+            Router router = new Router(s);
+            s.AddScenarioObject(router);
+            router.addRoutingRule(1, "isEven", ValueComparisonOperatorEnum.Equals, "true", evenLogger);
+            router.addRoutingRule(2, "isEven", ValueComparisonOperatorEnum.Equals, "false", hub);
+
+            s.subscriptionManager.AddSubscription(periodicTimer, rndIntGenerator);
+            s.subscriptionManager.AddSubscription(rndIntGenerator, router);
+            s.subscriptionManager.AddSubscription(router, evenLogger);
+            s.subscriptionManager.AddSubscription(router, hub);
+            s.subscriptionManager.AddSubscription(hub, oddLogger1);
+            s.subscriptionManager.AddSubscription(hub, oddLogger2);
+            s.subscriptionManager.AddSubscription(hub, oddLogger3);
+
+            s.Run();
+
+            Console.ReadLine();
+
+        }
+    }
+
+  
+    
+
+
+
+
+
+
+
+}
